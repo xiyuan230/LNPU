0a0b7ae27a75af1237cfa6983c3df8acb89d7f5f
xiyuan <xiyuan230@qq.com>
2023-03-07 22:23:54 +0800

    添加后台启动方式，参数 -d 开启后台启动

--- main.go
+++ main.go
@@ -3,17 +3,5 @@ package main
 import "MyLNPU/cmd"
 
 func main() {
-	//gin.SetMode(gin.ReleaseMode)
-	//r := gin.Default()
-	//conf.Init()   //配置初始化
-	//logger.Init() //日志初始化
-	//api.Init(r)   //路由初始化
-	//cache.Init()  //Redis初始化
-	//db.Init()     //Mysql初始化
-	//serverPort := ":" + strconv.Itoa(conf.GetConfig().Server.Port)
-	//logger.Println("MyLNPU启动成功... Port: %d", conf.GetConfig().Server.Port)
-	//if err := r.Run(serverPort); err != nil {
-	//	logger.Errorf(err.Error())
-	//}
 	cmd.Execute()
 }
